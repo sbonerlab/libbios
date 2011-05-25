7be6661c3112eeaf0f8400dcc0730f7d557c14b1
David Z. Chen <david.z.chen@yale.edu>
2011-05-25 23:21:25 +0000

    Set up pkg-config configuration and adjusted version and so version to 1.0.0
    git-svn-id: svn+ssh://svn.gersteinlab.org/repos/bioinfo/libbios@597 2fd4de08-f6cc-4d2b-b124-baeea9ba92f3

---
+++ autoclean.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo autoclean: cleaning root
+rm -rf aclocal.m4 \
+       autom4te.cache \
+       autoscan.log \
+       config.guess \
+       bios.pc \
+       config.h \
+       config.h.in~ \
+       config.log \
+       config.status \
+       config.sub \
+       configure \
+       depcomp \
+       install-sh \
+       libtool \
+       ltmain.sh \
+       missing \
+       Makefile.in \
+       Makefile \
+       stamp-h1
+
+echo autoclean: cleaning m4
+cd m4 && rm -rf \
+       libtool.m4 \
+       lt~obsolete.m4 \
+       ltoptions.m4 \
+       ltsugar.m4 \
+       ltversion.m4
+cd ..
+
+echo autoclean: cleaning lib
+cd lib && rm -rf \
+       Makefile.in \
+       Makefile
+cd ..
